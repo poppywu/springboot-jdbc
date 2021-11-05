5071fb9d493e91045f99bfe9f08ad2679b41498e
Poppy Ng <mmiswaiting@gmail.com>
2021-11-05 16:22:26 -0700

    init course project

---
+++ src/main/java/demo/coursejdbc/controller/CourseController.java
@@ -0,0 +1,11 @@
+package demo.coursejdbc.controller;
+
+import demo.coursejdbc.model.Course;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+}
---
+++ src/main/java/demo/coursejdbc/dao/CourseJdbcDAO.java
@@ -0,0 +1,66 @@
+package demo.coursejdbc.dao;
+
+import demo.coursejdbc.model.Course;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+
+import java.util.List;
+import java.util.Optional;
+@Component
+public class CourseJdbcDAO implements DAO<Course>{
+    private static final Logger log= LoggerFactory.getLogger(CourseJdbcDAO.class);
+    private JdbcTemplate jdbcTemplate;
+
+    RowMapper<Course> rowMapper=(rs,rowNum)->{
+        Course course=new Course();
+        course.setCourseId(rs.getInt("course_id"));
+        course.setTitle(rs.getString("title"));
+        course.setDescription((rs.getString("description")));
+        course.setLink((rs.getString("link")));
+        return course;
+    };
+
+    public CourseJdbcDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Course> list() {
+        String sql="SELECT course_id,title,description,link FROM course";
+        return jdbcTemplate.query(sql,rowMapper);
+    }
+
+    @Override
+    public void create(Course course) {
+        String sql="INSERT INTO course(title,description,link)values(?,?,?)";
+        jdbcTemplate.update(sql,course.getTitle(),course.getDescription(),course.getLink());
+    }
+
+    @Override
+    public Optional<Course> get(int id) {
+        String sql="SELECT course_id, title, description, link FROM course WHERE course_id=?";
+        Course course=null;
+        try{
+            course=jdbcTemplate.queryForObject(sql,new Object[] {id},rowMapper);
+        }catch(DataAccessException ex){
+            log.info("Course not found"+id);
+        }
+        return Optional.ofNullable(course);
+    }
+
+    @Override
+    public void update(Course course, int id) {
+        String sql="update course set title=?,description=?,link=? WHERE course_id=?";
+        int update=jdbcTemplate.update(sql,course.getTitle(),course.getDescription(),course.getLink(),id);
+    }
+
+    @Override
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM course WHERE course_id=?",id);
+    }
+}
